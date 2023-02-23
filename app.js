6cf7972439e645ffc2454cdf67ef6f7ba1bc4d66
Duong Hoai Bao <dhbao2505@gmail.com>
2023-02-23 21:51:44 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 
 app.get("/", (req, res) => {
     res.json({message: "Welcome to contact book application."});
