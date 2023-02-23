157df90f74b7d7038e57e04e1bac03b52acbfd8c
Duong Hoai Bao <dhbao2505@gmail.com>
2023-02-24 01:36:14 +0700

    Them chuc nang dang ky va dang nhap

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const ApiError = require("./app/api-error");
 
 const app = express();
 
+const users = [];
+
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
@@ -13,14 +15,43 @@ app.get("/", (req, res) => {
     res.json({message: "Welcome to contact book application."});
 });
 
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Missing required information' });
+    }
+
+    const existingUser = users.find(user => user.username === username);
+    if (existingUser) {
+        return res.status(409).json({ message: 'Username already exists' });
+    }
+
+    const newUser = { id: users.length + 1, username, password };
+     users.push(newUser);
+
+    return res.status(201).json({ message: 'Registration successful' });
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    const user = users.find(user => user.username === username && user.password === password);
+
+    if (user) {
+        return res.status(200).json({ message: 'Login successful' });
+    } else {
+        return res.status(401).json({ message: 'Invalid username or password' });
+    }
+});
+
 app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
 });
-app.use((error, req, res, next) => {
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
     });
 });
 
-module.exports = app;
 module.exports = app;
