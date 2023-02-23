23f74791a1bcf618839da5624149765364be9aa2
Duong Hoai Bao <dhbao2505@gmail.com>
2023-02-23 21:09:54 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
     res.json({message: "Welcome to contact book application."});
 });
 
+module.exports = app;
 module.exports = app;
