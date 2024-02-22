f722941120a4a986e743ca39e27e464cb8245035
dhbao-dev <dhbao2505@gmail.com>
2024-02-22 13:34:49 +0700

    update

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -30,7 +30,7 @@ exports.findAll = async (req, res, next) => {
             document = await contactService.find({});
         }
     } catch(error) {
-        return next( console.log(error)
+        return next( 
             new ApiError(500, "An error occurred while retrieving contacts")
         );
     }
--- app/utils/mongodb.util.js
+++ app/utils/mongodb.util.js
@@ -1,9 +1,15 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 class MongoDB {
     static connect = async (uri) => {
         if (this.client) return this.client;
-        this.client = await MongoClient.connect(uri);
+        this.client = await MongoClient.connect(uri, {
+            serverApi: {
+              version: ServerApiVersion.v1,
+              strict: true,
+              deprecationErrors: true,
+            }
+          });
         return this.client;
     };
 }
