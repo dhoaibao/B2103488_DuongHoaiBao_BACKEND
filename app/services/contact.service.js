4618661da67ba465fa68748238bb7179c0b8e260
dhbao-dev <dhbao2505@gmail.com>
2024-02-22 13:56:26 +0700

    update

--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -2,7 +2,7 @@ const { ObjectId } = require("mongodb");
 
 class ContactService {
     constructor(client) {
-        this.Contact = client.db().collection("contacts");
+        this.Contact = client.db("admin").collection("contacts");
     }
 
     extractContactData(payload) {
