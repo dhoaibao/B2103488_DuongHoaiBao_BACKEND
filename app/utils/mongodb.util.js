cdb5c1842b585bd9646943837857b47456e2b2ae
dhbao-dev <dhbao2505@gmail.com>
2024-02-22 13:54:43 +0700

    update

--- app/utils/mongodb.util.js
+++ app/utils/mongodb.util.js
@@ -1,10 +1,20 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 class MongoDB {
     static connect = async (uri) => {
-        if (this.client) return this.client;
-        this.client = await MongoClient.connect(uri);
-        return this.client;
+        // if (this.client) return this.client;
+        // this.client = await MongoClient.connect(uri);
+        // return this.client;
+        const client = new MongoClient(uri, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            }
+        });
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     };
 }
 
