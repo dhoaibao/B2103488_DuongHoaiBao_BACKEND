a658aba4453dcad8b9432232074f34e081c38768
dhbao-dev <dhbao2505@gmail.com>
2024-02-22 14:03:07 +0700

    update

--- app/utils/mongodb.util.js
+++ app/utils/mongodb.util.js
@@ -3,19 +3,7 @@ const { MongoClient } = require("mongodb");
 class MongoDB {
     static connect = async (uri) => {
         if (this.client) return this.client;
-        this.client = await MongoClient.connect(uri,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        },(err,connectedClient) => {
-            if(err){
-                throw err;
-            }
-            //connectedClient will be the connected instance of MongoClient
-            this.client = connectedClient.db("test");
-            //now you can write queries
-            
-        
-        });
+        this.client = await MongoClient.connect(uri);
         return this.client;
     };
 }
