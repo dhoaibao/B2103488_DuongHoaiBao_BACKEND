c15f4358ffc9b969bf8c48cf366e7a7398c32ed3
dhbao-dev <dhbao2505@gmail.com>
2024-02-22 14:11:55 +0700

    update

--- app/utils/mongodb.util.js
+++ app/utils/mongodb.util.js
@@ -1,11 +1,21 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 class MongoDB {
     static connect = async (uri) => {
-        if (this.client) return this.client;
-        this.client = await MongoClient.connect(uri);
-        return this.client;
+        const clientBAO = new MongoClient(uri, {
+            serverApi: {
+              version: ServerApiVersion.v1,
+              strict: true,
+              deprecationErrors: true,
+            }
+          });
+        
+        this.client = await clientBAO.connect(uri);
+        
+        return clientBAO;
     };
 }
 
 module.exports = MongoDB;
+
+
