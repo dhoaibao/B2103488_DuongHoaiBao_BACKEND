2c6693c06ff84e9fe0789588e71c58bfe6deb337
dhbao-dev <dhbao2505@gmail.com>
2024-02-22 13:48:35 +0700

    update

--- app/utils/mongodb.util.js
+++ app/utils/mongodb.util.js
@@ -1,17 +1,9 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient } = require("mongodb");
 
 class MongoDB {
     static connect = async (uri) => {
         if (this.client) return this.client;
-        this.client = await MongoClient.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }, (err, connectedClient) => {
-            if (err) {
-                throw err;
-            }
-            this.client = connectedClient.db("test");
-        });
+        this.client = await MongoClient.connect(uri);
         return this.client;
     };
 }
