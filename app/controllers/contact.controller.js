e379cb031c110fb8923f3e3cff98b5e0a1f1ecf6
dhbao-dev <dhbao2505@gmail.com>
2024-02-22 14:00:50 +0700

    update

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -31,7 +31,7 @@ exports.findAll = async (req, res, next) => {
         }
     } catch(error) {
         return next( 
-            new ApiError(500, `An error occurred while retrieving contacts BAO3: ${error}`)
+            new ApiError(500, `An error occurred while retrieving contacts BAO4: ${error}`)
         );
     }
 
--- app/utils/mongodb.util.js
+++ app/utils/mongodb.util.js
@@ -3,7 +3,25 @@ const { MongoClient } = require("mongodb");
 class MongoDB {
     static connect = async (uri) => {
         if (this.client) return this.client;
-        this.client = await MongoClient.connect(uri);
+        this.client = await MongoClient.connect(uri,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        },(err,connectedClient) => {
+            if(err){
+                throw err;
+            }
+            //connectedClient will be the connected instance of MongoClient
+            this.client = connectedClient.db("test");
+            //now you can write queries
+        
+            db.collection("contacts").find({}).toArray()
+            .then(r => {
+                console.log(r);
+            }).catch(e => {
+                console.error(`ERROR:`,e);
+            })
+        
+        });
         return this.client;
     };
 }
